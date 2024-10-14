fc6e1814a4ddb67963e418b2b646d5aa6f0ae866
mattersen <mattersen@gmail.com>
2024-10-14 12:42:30 -0700

    add ability of player to shoot

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from circleshape import CircleShape
+from shot import Shot
 
 def main():
     pygame.init()
@@ -17,10 +18,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -52,11 +55,6 @@ def main():
 
         # limits frame rate to 60 fps
         dt = clock.tick(60) / 1000 
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
